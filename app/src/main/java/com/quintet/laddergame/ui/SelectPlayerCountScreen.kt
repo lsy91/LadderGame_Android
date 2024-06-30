cd329a5c6bc334f8fad61adc222f7549d79489e9
이수용 <2sy.lee@quintet.co.kr>
2024-06-30 15:53:22 +0900

    [sy.lee] Modify Design

--- app/src/main/java/com/quintet/laddergame/ui/SelectPlayerCountScreen.kt
+++ app/src/main/java/com/quintet/laddergame/ui/SelectPlayerCountScreen.kt
@@ -2,20 +2,25 @@ package com.quintet.laddergame.ui
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -24,10 +29,14 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.quintet.laddergame.ui.theme.LightPrimaryVariant
 
 /**
  * Select Player Count Screen
@@ -65,13 +74,16 @@ fun SelectPlayerCountScreen(
             item {
                 Row(
                     modifier = Modifier.fillMaxWidth(),
-                    verticalAlignment = Alignment.CenterVertically
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
                 ) {
                     Text(
                         text = "게임 인원 수",
                         fontSize = 20.sp,
                         style = MaterialTheme.typography.titleMedium,
-                        modifier = Modifier.weight(1f)
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .wrapContentSize()
                     )
 
                     Spacer(modifier = Modifier.width(10.dp))
@@ -87,13 +99,19 @@ fun SelectPlayerCountScreen(
                                 }
                             }
                         },
+                        textStyle = LocalTextStyle.current.copy(textAlign = TextAlign.Center),
                         keyboardOptions = KeyboardOptions.Default.copy(
                             keyboardType = KeyboardType.Number,
                             imeAction = ImeAction.Done
                         ),
-                        modifier = Modifier.weight(1f)
+                        colors = TextFieldDefaults.colors(
+                            focusedContainerColor = Color.Transparent, // 포커스된 배경색 투명으로 설정
+                            unfocusedContainerColor = Color.Transparent // 포커스 해제된 배경색 투명으로 설정
+                        )
                     )
                 }
+
+                Spacer(modifier = Modifier.height(20.dp))
             }
 
             // 플레이어 이름 입력 필드
--- app/src/main/java/com/quintet/laddergame/ui/SelectWinnerCountScreen.kt
+++ app/src/main/java/com/quintet/laddergame/ui/SelectWinnerCountScreen.kt
@@ -7,15 +7,19 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -24,10 +28,14 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.quintet.laddergame.ui.theme.LightPrimaryVariant
 
 /**
  * Select Winner Count Screen
@@ -66,13 +74,16 @@ fun SelectWinnerCountScreen(
             item {
                 Row(
                     modifier = Modifier.fillMaxWidth(),
-                    verticalAlignment = Alignment.CenterVertically
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
                 ) {
                     Text(
                         text = "당첨 개수",
                         fontSize = 20.sp,
                         style = MaterialTheme.typography.titleMedium,
-                        modifier = Modifier.weight(1f)
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .wrapContentSize()
                     )
 
                     Spacer(modifier = Modifier.width(10.dp))
@@ -88,13 +99,19 @@ fun SelectWinnerCountScreen(
                                 }
                             }
                         },
+                        textStyle = LocalTextStyle.current.copy(textAlign = TextAlign.Center),
                         keyboardOptions = KeyboardOptions.Default.copy(
                             keyboardType = KeyboardType.Number,
                             imeAction = ImeAction.Done
                         ),
-                        modifier = Modifier.weight(1f)
+                        colors = TextFieldDefaults.colors(
+                            focusedContainerColor = Color.Transparent, // 포커스된 배경색 투명으로 설정
+                            unfocusedContainerColor = Color.Transparent // 포커스 해제된 배경색 투명으로 설정
+                        )
                     )
                 }
+
+                Spacer(modifier = Modifier.height(20.dp))
             }
 
             // 당첨 제목 입력 필드
