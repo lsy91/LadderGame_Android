c35b50116c6d01d4e8125e46733b43b16ed5eb7c
sy.lee <2sy.lee@quintet.co.kr>
2024-11-28 19:02:04 +0900

    [sy.lee] MVI 아키텍처 적용 1. Map Extension Utils 함수 생성

--- app/src/main/java/com/quintet/laddergame/utils/CollectionExtensions.kt
+++ app/src/main/java/com/quintet/laddergame/utils/CollectionExtensions.kt
@@ -1,2 +1,16 @@
 package com.quintet.laddergame.utils
 
+/**
+ * 요소가 없으면 추가, 있으면 제거하는 Map Extension Function
+ *
+ * @param E
+ * @param element
+ * @return
+ */
+internal fun <E> Set<E>.addOrRemove(element: E): Set<E> {
+    return this.toMutableSet().apply {
+        if (!add(element)) {
+            remove(element)
+        }
+    }.toSet()
+}
