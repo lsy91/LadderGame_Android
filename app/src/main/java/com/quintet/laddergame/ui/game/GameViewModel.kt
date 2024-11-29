746e575280ef5305b2510b8a2fb444f3c67bc9c7
sy.lee <2sy.lee@quintet.co.kr>
2024-11-29 17:00:23 +0900

    [sy.lee] MVI 아키텍처 적용 1. Application 클래스, MainActivity 에 Hilt 설정 누락된거 추가

--- app/src/main/java/com/quintet/laddergame/ui/game/GameViewModel.kt
+++ app/src/main/java/com/quintet/laddergame/ui/game/GameViewModel.kt
@@ -2,8 +2,9 @@ package com.quintet.laddergame.ui.game
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class GameViewModel: ViewModel() {
+class GameViewModel @Inject constructor(): ViewModel() {
 
 }
--- app/src/main/java/com/quintet/laddergame/ui/player/PlayerViewModel.kt
+++ app/src/main/java/com/quintet/laddergame/ui/player/PlayerViewModel.kt
@@ -2,8 +2,9 @@ package com.quintet.laddergame.ui.player
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class PlayerViewModel: ViewModel() {
+class PlayerViewModel @Inject constructor(): ViewModel() {
 
 }
